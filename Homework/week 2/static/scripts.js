985ca8f88068e96e0c73c882b46a2147f03f263e
Troy C. Breijaert <troy.breijaert@gmail.com>
2017-04-10 12:54:00 +0200

    Week 2 dag 1 uur 1

---
+++ Homework/week 2/static/scripts.js
@@ -0,0 +1,14 @@
+function(){
+  var rawdata = document.getElementById('rawdata')
+  var formdata = [];
+  for (var i = 0; i < rawdata.length; i++){
+    k = rawdata.split(",");
+    formdata.push("k");
+  }
+  console.log(errorThrown.toString());
+}
+
+//function canvas(){
+  //var canvas = document.getElementById('myCanvas');
+  //var ctx = canvas.getContext('2d');
+//}
