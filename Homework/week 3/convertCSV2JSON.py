51eaf33580cd4063fd26bf9e71820ea8df8b39a2
Troy C. Breijaert <troy.breijaert@gmail.com>
2017-04-20 15:57:27 +0200

    Update 1

--- Homework/week 3/convertCSV2JSON.py
+++ Homework/week 3/convertCSV2JSON.py
@@ -5,7 +5,7 @@ csvFile = open('..\DataProcessing\Homework\week 3\KNMI.csv', 'r')
 jsonFile = open('..\DataProcessing\Homework\week 3\KNMI.JSON', 'w')
 reader = csv.reader(filter(lambda row: row[0]!='#', csvFile))
 for row in reader:
-    json.dump({'date': row[1].strip(), 'rainfall':row[2].strip()}, jsonFile)
+    json.dump({'date': row[1].strip(), 'rainfall':int(row[2].strip())}, jsonFile)
     jsonFile.write('\n')
 csvFile.close()
 jsonFile.close()
